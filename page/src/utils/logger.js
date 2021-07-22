245b2a8b76baa4fef525b87cdcc40ad17f70ee61
Gj <gongjie0422@163.com>
2021-07-22 15:07:55 +0800

    fix: page.traceId

--- page/src/utils/logger.js
+++ page/src/utils/logger.js
@@ -1,6 +1,7 @@
 import { mount } from "@beeweb/logger";
 
 mount({
+    traceId: "dynamic-router",
     serverURL: 'http://logger.alrale.cn',
     mapURI: "http://api.map.baidu.com/location/ip?ak=RD3fQS8GA1UeR4Ig10ejdEkTg1OfwuV3",
     encryptionFunc: 'useDefault'
